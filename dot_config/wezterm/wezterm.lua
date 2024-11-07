91664a1780ca1532145fef4f79c800e8fc3c5a70
Cian Hughes <chughes000@gmail.com>
2024-11-07 16:39:16 +0000

    Updated weaterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.enable_wayland = false
+config.enable_wayland = true
 config.default_prog = { "nu" }
 config.color_scheme = "Tokyo Night"
 config.enable_kitty_keyboard = true
 config.font = wezterm.font("MonaspiceNeNerdFontMono")
 config.font_size = 16
+config.use_fancy_tab_bar = false
 
 return config
