fc23acb8761d007f0e0e2ee5c2cb4fbb91cf9b96
Cian Hughes <chughes000@gmail.com>
2024-11-07 18:10:55 +0000

    Increase wezterm max_fps

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.enable_wayland = true
+config.max_fps = 120
 config.default_prog = { "nu" }
 config.color_scheme = "Tokyo Night"
 config.enable_kitty_keyboard = true
