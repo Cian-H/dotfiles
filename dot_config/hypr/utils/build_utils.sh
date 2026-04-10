7ad4e33b37234a3af84d1c3239636dae23a85625
Cian Hughes <chughes000@gmail.com>
2026-04-10 09:57:20 +0100

    Simplified hyprland config

--- dot_config/hypr/utils/build_utils.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-SCRIPT_PATH="$(realpath "$0")"
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-
-cd "$SCRIPT_DIR"
-
-go build -o ./cycle_kb_layout ./src/cycle_kb_layout.go
--- dot_config/hypr/utils/src/go.mod
+++
@@ -1,3 +0,0 @@
-module cycle_kb_layout
-
-go 1.24.4
